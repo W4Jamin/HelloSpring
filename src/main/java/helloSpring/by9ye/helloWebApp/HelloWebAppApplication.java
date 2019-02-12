4f5200ea0164d83dbd3cf534f0c353aa43481605
Bin Yin <by9ye@virginia.edu>
2019-02-12 15:11:31 -0500

    Add notation

---
+++ src/main/java/helloSpring/by9ye/helloWebApp/HelloWebAppApplication.java
@@ -0,0 +1,14 @@
+package helloSpring.by9ye.helloWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWebAppApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/helloSpring/by9ye/helloWebApp/HelloWebAppApplicationTests.java
@@ -0,0 +1,17 @@
+package helloSpring.by9ye.helloWebApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelloWebAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
