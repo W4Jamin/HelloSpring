5ff6001aeb054d4b91ed5fbf67cc77ea6ebf7343
Bin Yin <by9ye@virginia.edu>
2019-02-15 15:13:25 -0500

    Use Primary

--- src/main/java/by9ye/springframework/didemo/controllers/MyController.java
+++ src/main/java/by9ye/springframework/didemo/controllers/MyController.java
@@ -1,12 +1,19 @@
 package by9ye.springframework.didemo.controllers;
 
+import by9ye.springframework.didemo.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
 
+    private GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
     public String hello() {
         System.out.println("hello!!");
-        return "foo";
+        return greetingService.sayGreeting();
     }
 }
--- src/main/java/by9ye/springframework/didemo/controllers/PropertyInjectedController.java
+++ src/main/java/by9ye/springframework/didemo/controllers/PropertyInjectedController.java
@@ -2,12 +2,14 @@ package by9ye.springframework.didemo.controllers;
 
 import by9ye.springframework.didemo.services.GreetingServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class PropertyInjectedController {
 
     @Autowired
+    @Qualifier("greetingServiceImpl")
     public GreetingServiceImpl greetingService;
 
     public String sayHello(){
