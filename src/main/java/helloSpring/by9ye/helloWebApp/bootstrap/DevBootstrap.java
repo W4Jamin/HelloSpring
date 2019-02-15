b74fe50584437d7783dd617d6faada00d42d7a2d
Bin Yin <by9ye@virginia.edu>
2019-02-14 21:31:27 -0500

    Add a new model Publisher and a controller for getting books

--- src/main/java/helloSpring/by9ye/helloWebApp/bootstrap/DevBootstrap.java
+++ src/main/java/helloSpring/by9ye/helloWebApp/bootstrap/DevBootstrap.java
@@ -2,8 +2,10 @@ package helloSpring.by9ye.helloWebApp.bootstrap;
 
 import helloSpring.by9ye.helloWebApp.model.Author;
 import helloSpring.by9ye.helloWebApp.model.Book;
+import helloSpring.by9ye.helloWebApp.model.Publisher;
 import helloSpring.by9ye.helloWebApp.repositories.AuthorRepo;
 import helloSpring.by9ye.helloWebApp.repositories.BookRepo;
+import helloSpring.by9ye.helloWebApp.repositories.PublisherRepo;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -12,10 +14,12 @@ import org.springframework.stereotype.Component;
 public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> { // 事件监听
     private AuthorRepo authorRepo;
     private BookRepo bookRepo;
+    private PublisherRepo publisherRepo;
 
-    public DevBootstrap(AuthorRepo authorRepo, BookRepo bookRepo) {
+    public DevBootstrap(AuthorRepo authorRepo, BookRepo bookRepo, PublisherRepo publisherRepo) {
         this.authorRepo = authorRepo;
         this.bookRepo = bookRepo;
+        this.publisherRepo = publisherRepo;
     }
 
     @Override
@@ -27,7 +31,11 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
         //Eric
         Author eric = new Author("Eric", "Evans");
-        Book ddd = new Book("Domain Driven Design", "1234", "Harper Collins");
+        Publisher pub_ddd = new Publisher();
+        pub_ddd.setName("Bin");
+        pub_ddd.setAddress("474 Seymour Rd");
+        publisherRepo.save(pub_ddd);
+        Book ddd = new Book("Domain Driven Design", "1234", pub_ddd);
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
@@ -35,13 +43,17 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
         bookRepo.save(ddd);
 
 
+
         //Rod
         Author rod = new Author("Rod", "Johnson");
-        Book noEJB = new Book("J2EE Development without EJB", "23444", "Wrox" );
+        Publisher pub_rot = new Publisher("Jamin", "Finkenstr 64");
+        publisherRepo.save(pub_rot);
+        Book noEJB = new Book("J2EE Development without EJB", "23444", pub_rot );
         rod.getBooks().add(noEJB);
         noEJB.getAuthors().add(rod);
 
         authorRepo.save(rod);
         bookRepo.save(noEJB);
+
     }
 }
---
+++ src/main/java/helloSpring/by9ye/helloWebApp/controller/BookController.java
@@ -0,0 +1,23 @@
+package helloSpring.by9ye.helloWebApp.controller;
+
+import helloSpring.by9ye.helloWebApp.repositories.BookRepo;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+
+    private BookRepo bookRepo;
+
+    public BookController(BookRepo bookRepo) {
+        this.bookRepo = bookRepo;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+        model.addAttribute("books", bookRepo.findAll());
+
+        return "books"; // for Thymleaf
+    }
+}
